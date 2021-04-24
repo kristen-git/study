d79bc8f26cf875803d3d56d5ffcf3f37bdeb04f3
zhuangxin <18795975110@163.com>
2021-04-24 19:34:49 +0800

    first

--- name.py
+++ name.py
@@ -1,6 +1,8 @@
 import math
 a=int(input("请输入一个数字"))
-if a>10:
-    print("此数字大于10")
+if a<10:
+    print("此数字小于10")
+elif 10<=a<=20:
+    print('10-20')
 else:
-    print("1-10")
+    print("此数字大于20")
